1b2ebc6e5146769c8d4847d40f6569a3d04ab804
KHJ-11 <72050086+KHJ-11@users.noreply.github.com>
2023-09-13 19:44:53 +0900

    유저 검색
    2023.09.13

--- FC_Online/app/src/main/java/com/example/fc_online/ui/fragment/HomeScreen_01.kt
+++ FC_Online/app/src/main/java/com/example/fc_online/ui/fragment/HomeScreen_01.kt
@@ -5,13 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.Navigation
 import com.example.fc_online.R
+import com.example.fc_online.data.UserInfo
+import com.example.fc_online.databinding.FragmentHomeScreen01Binding
+import com.example.fc_online.util.Constants.KEY
+import com.example.fc_online.util.Constants.api
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class HomeScreen_01 : Fragment() {
+    private lateinit var binding: FragmentHomeScreen01Binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen_01, container, false)
+        savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeScreen01Binding.inflate(layoutInflater)
+        val view = binding.root
+
+        userEditText()
+
+        return view
+    }
+
+    private fun userEditText() {
+        binding.homeButton.setOnClickListener {
+            getUserInfo()
+        }
+    }
+
+    private fun getUserInfo() {
+        val callGetUserInfo = api.getUserInfo("${KEY}", "${binding.homeEditText.text.toString()}")
+
+        callGetUserInfo.enqueue(object : Callback<UserInfo> {
+            override fun onResponse(call: Call<UserInfo>, response: Response<UserInfo>) {
+                val user = response.body()
+
+                if (binding.homeEditText.text.toString() != user?.nickname) {
+                    Navigation.findNavController(binding.root).navigate(R.id.action_homeScreen_01_to_userInfo_02)
+                } else {
+                    val bundle = bundleOf(
+                        "nickname" to user?.nickname,
+                        "level" to user?.level,
+                        "accessid" to user?.accessId
+                    )
+
+                }
+            }
+
+            override fun onFailure(call: Call<UserInfo>, t: Throwable) {
+
+            }
+
+        })
     }
 }
