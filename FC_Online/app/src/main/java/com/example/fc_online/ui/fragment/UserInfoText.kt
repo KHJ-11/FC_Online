9f380506fabb4b176e76980ce8462c45337cd39e
KHJ-11 <72050086+KHJ-11@users.noreply.github.com>
2023-10-26 14:56:01 +0900

    즐겨찾기
    2023-10-26

--- FC_Online/app/src/main/java/com/example/fc_online/ui/fragment/UserInfoText.kt
+++ FC_Online/app/src/main/java/com/example/fc_online/ui/fragment/UserInfoText.kt
@@ -1,5 +1,7 @@
 package com.example.fc_online.ui.fragment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +15,7 @@ import com.example.fc_online.databinding.FragmentUserInfoTextBinding
 class UserInfoText : Fragment() {
     private lateinit var binding: FragmentUserInfoTextBinding
     private val sharedViewModel: SharedViewModel by activityViewModels()
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
@@ -31,6 +34,33 @@ class UserInfoText : Fragment() {
         binding.userTextLevel.text = "Lv ${arguments?.getInt("level")}"
 //        binding.userTextAccessid.text = sharedViewModel.sharedData
 
+        binding.saveButton.setOnClickListener {
+            val saveName = binding.userTextName.text.toString()
+            if (isFavorite(saveName)) {
+                removeFavorite(saveName)
+
+            } else {
+                addFavorite(saveName)
+
+            }
+        }
+
+    }
+
+    private fun addFavorite(text: String) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean(text, true)
+        editor.apply()
+    }
+
+    private fun removeFavorite(text: String) {
+        val editor = sharedPreferences.edit()
+        editor.remove(text)
+        editor.apply()
+    }
+
+    private fun isFavorite(text: String): Boolean {
+        return sharedPreferences.getBoolean(text, false)
     }
 
 }
